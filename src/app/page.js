f5db21f9348d122cf28204edbd6fd634824c57d1
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-28 21:41:35 +0530

    Event Handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,12 @@
-
+"use client"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Home page</h1>
+      <h1>Event Function calling</h1>
 
-      <User name="Nilesh Choudhary" />
-      <User name="Ankit" />
-      <User name="Chandu" />
-      <User name="Lokesh" />
-      <User name="Aayush" />
+      <button className="bg-[red] p-3 text-white rounded-xl" onClick={() => alert("Hello Brother")}>Click</button>
     </main>
   );
 }
 
 
-const User = (props) => {
-  return (
-    <div>
-      <h2>User name is {props.name}</h2>
-    </div>
-  )
-}
