abd8d3348b320c50f631fc66e895a6d5854c9701
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-29 12:45:55 +0530

    useRouter UseNavigation Hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,20 @@
+"use client"
 import Link from "next/link";
-import { NavigateAction } from "next/dist/client/components/router-reducer/router-reducer-types";
+import { useRouter } from "next/navigation";
 export default function Home() {
+
+  const router = useRouter();
   return (
     <main className="bg-red">
       <h1>Basic Routing | make New Page</h1>
       <Link href="/Login">Go to login page</Link>
       <br />
       <Link href="/about">Go to About page</Link>
+
+      <br />
+      <br />
+
+      <button onClick={() => router.push("/Login")}>Go to Login Pgae</button>
     </main>
   )
 }
