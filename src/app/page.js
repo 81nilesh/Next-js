fd998c79de120583d11a4376be8e5f5a80103c03
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-29 12:29:16 +0530

    Routing in Next JS

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,7 @@
-"use client"
 export default function Home() {
-
-  const apple = (item) => {
-    alert(item);
-  }
-
-  const nilu = () => {
-    return (
-      <h1>Hello Brother</h1>
-    )
-  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Event Function calling</h1>
-      <button className="bg-[red] p-3 text-white rounded-xl" onClick={() => apple("color")}>Click</button>
-      <nilu />
-      {nilu()}
+    <main className="bg-red">
+      <h1>Basic Routing | make New Page</h1>
     </main>
-  );
+  )
 }
-
-
